1246fc96bc2826e8af0346d04f3450f97d685a08
Eligio Biasi <ex_98@live.it>
2023-06-21 17:18:21 +0200

    milestone 1 completed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,77 @@
 <template>
-   <SearchApp />
+    <div class="container">
+        <SearchApp @addMessage="apiCall"/>
+
+        <div class="flex">
+            <div class="film-card" v-for="film in FilmList">
+                <h3>
+                    {{ film.title}}
+                </h3>
+
+                <p>
+                    {{ film.original_title}}
+                </p>
+
+                <p>
+                    {{ film.original_language}}
+                </p>
+
+                <p>
+                    {{ film.vote_average.toFixed(0) }}
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import SearchApp from './SearchApp.vue';
+import axios from 'axios';
 
 export default {
     name:'AppMain',
 
+    data(){
+        return{
+            ApiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=6b2e78461e0d04629d8506dcedd436c4&query=',
+            FilmList : [],
+        }
+    },
+
     components:{
         SearchApp,
-    }
+    },
+
+    methods:{
+        apiCall(call){
+            axios.get(this.ApiUrl + call)
+                .then( (response) => {
+                    this.FilmList = response.data.results;
+                });
+        }
+    },
+    
+
 }
 </script>
 
 <style lang="scss" scoped>
-    
+    div.container{
+        width: 1200px;
+        margin: 0 auto;
+    }
+
+    div.film-card{
+        width: calc((100%/5) - 2.13rem);
+        border: solid black 1px;
+        margin-right: 0.5rem;
+        margin-bottom: 0.5rem;
+        padding: 0 .5rem;
+    }
+
+    div.flex{
+        display: flex;
+        flex-wrap: wrap;
+    }
+
 </style>
--- src/components/SearchApp.vue
+++ src/components/SearchApp.vue
@@ -1,42 +1,40 @@
 <template>
-    <div class="flex">
-        <div class="film-card" v-for="film in FilmList">
-            {{ film.original_title }}
-        </div>
+    <div>
+        <input type="text" placeholder="Search here" v-model="querySearch">
+        <button @click="$emit('addMessage', querySearch)">Search</button>
     </div>
+    
 </template>
 
 <script>
-import axios from 'axios';
 
 export default {
     data(){
         return {
-            QuerySearch : "",
-            FilmList : [],
+            querySearch : "",
         }
     },
 
-    created(){
-    axios.get('https://api.themoviedb.org/3/search/movie?api_key=6b2e78461e0d04629d8506dcedd436c4&query=back')
-        .then( (response) => {
-            this.FilmList = response.data.results;
-            console.log(response.data.results)
-        });
+    methods:{
+    
     }
 }
 </script>
 
 <style lang="scss" scoped>
-    div.film-card{
-        width: calc((100%/5) - 1.14rem);
-        height: 200px;
-        border: solid black 1px;
-        margin: 0.5rem;
+   
+    input{
+        margin: .5rem;
+        border-radius: 5px;
+        border: solid 1px magenta;
+        width: 200px;
+        padding: .5rem;
     }
 
-    div.flex{
-        display: flex;
-        flex-wrap: wrap;
+    button{
+        border-radius: 5px;
+        border: solid 1px magenta;
+        background-color: lime;
+        padding: .5rem;
     }
 </style>

8ea3cf938efdac7516bf1e9e4528eecac0c53921
Eligio Biasi <ex_98@live.it>
2023-06-22 15:08:57 +0200

    added tv series

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="container">
       <SearchApp @addMessage="apiCall" />
-  
+      <h1>
+          Film
+      </h1>
       <div class="flex">
             <div class="film-card" v-for="film in FilmList" :key="film.id">
                 <h3>
@@ -19,6 +21,27 @@
                 </p>
             </div>
       </div>
+      
+      <div>
+        <h1>
+          TV series
+        </h1>
+        <div class="flex">
+            <div class="film-card" v-for="series in seriesList">
+                <h3>
+                    {{ series.name }}
+                </h3>
+
+                <p>
+                    {{ series.original_name }}
+                </p>
+
+                <p>
+                    {{ series.vote_average.toFixed(0) }}
+                </p>
+            </div>
+        </div>
+      </div>
     </div>
   </template>
   
@@ -32,6 +55,8 @@
       return {
         ApiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=6b2e78461e0d04629d8506dcedd436c4&query=',
         FilmList: [],
+        seriesList:[],
+        ApiSeriesUrl: 'https://api.themoviedb.org/3/search/tv?api_key=6b2e78461e0d04629d8506dcedd436c4&query=',
       };
     },
     components: {
@@ -43,11 +68,17 @@
           .then((response) => {
             this.FilmList = response.data.results;
           });
+
+        axios.get(this.ApiSeriesUrl + call)
+          .then((response) => {
+            this.seriesList= response.data.results;
+          });
       },
+      
       getFlagUrl(isoCode) {
         const flagFileName = `${isoCode.toLowerCase()}.png`;
 
-        if(`${isoCode.toLowerCase()}.png` === `../image/${flagFileName}`){
+        if(`${isoCode.toLowerCase()}.png` == `../image/${flagFileName}`){
             return (`../image/${flagFileName}`);
         }else{
             return (`${isoCode.toLowerCase()}`);
@@ -64,7 +95,7 @@
     }
 
     div.film-card{
-        width: calc((100%/5) - 2.13rem);
+        width: calc((100%/5) - 1.63rem);
         border: solid black 1px;
         margin-right: 0.5rem;
         margin-bottom: 0.5rem;
